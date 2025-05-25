94ef0fd635b0051fe7f8faf4d33c432b663d1c68
Doodleboo <174210143+Doodleboo@users.noreply.github.com>
2025-05-25 14:40:37 +0200

    Restrict buttons to one user

--- bot/tutorial_mode.py
+++ bot/tutorial_mode.py
@@ -1,7 +1,7 @@
 import discord
 from discord import Member, User, Thread, TextChannel, DMChannel
 from discord.ui import View, Button
-from discord import ButtonStyle
+from discord import ButtonStyle, Interaction
 
 SPRITER_ROLE_ID = 392803830900850688
 MANAGER_ROLE_ID = 900867033175040101
@@ -24,20 +24,40 @@ async def user_is_potential_spriter(user: User|Member) -> bool:
 async def send_tutorial_mode_prompt(user: Member, channel: TextChannel|Thread|DMChannel):
     prompt_text = (f"**Hi {user.display_name}!** If you're unsure what some of that means (for instance, "
                    f"similarity is probably not what you think!), press the **Tutorial Mode** button below.")
-    prompt_view = PromptButtonsView()
+    prompt_view = PromptButtonsView(user)
     await channel.send(content=prompt_text, view=prompt_view)
 
 
+# Views
+
 class PromptButtonsView(View):
 
+    def __init__(self, caller: Member):
+        self.original_caller = caller
+        super().__init__()
+
     @discord.ui.button(label="Tutorial Mode", style=ButtonStyle.primary, emoji="✏")
-    async def engage_tutorial_mode(self, interaction: discord.Interaction, button: Button):
-        await interaction.response.edit_message(content="New state")
+    async def engage_tutorial_mode(self, interaction: Interaction, button: Button):
+        #if interaction.user.id == self.original_caller.id:
+        #    await interaction.response.edit_message(content="New state")
+        #else:
+            await different_user_response(interaction, self.original_caller)
 
     @discord.ui.button(label="Discard", style=ButtonStyle.secondary)
-    async def discard_tutorial_prompt(self, interaction: discord.Interaction, button: Button):
-        await interaction.message.delete()
+    async def discard_tutorial_prompt(self, interaction: Interaction, button: Button):
+        #if interaction.user.id == self.original_caller.id:
+        #    await interaction.message.delete()
+        #else:
+            await different_user_response(interaction, self.original_caller)
 
+
+
+# View-related functions
+
+async def different_user_response(interaction: Interaction, og_user: Member):
+    response_text = (f"Hi {interaction.user.mention}! That's meant for {og_user.name}, but if you want to use "
+                     f"the Tutorial Mode yourself, you can use /help in a channel such as #ask_a_bot to do so.")
+    await interaction.response.send_message(content=response_text, ephemeral=True, delete_after=60)
 
 
 
