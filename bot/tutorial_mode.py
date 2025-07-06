b93b26c156c62c47af396e19e3aea9c8e533c537
Doodleboo <174210143+Doodleboo@users.noreply.github.com>
2025-07-06 14:57:20 +0200

    Log tweaks

--- bot/tutorial_mode.py
+++ bot/tutorial_mode.py
@@ -127,9 +127,9 @@ class TutorialMode(View):
             except (HTTPException, Forbidden, NotFound, TypeError) as error:
                 error_log = f"Exception {error} while trying to timeout Tutorial Mode"
                 if self.message.thread:
-                    error_log = error_log +  f"in {self.message.thread.name}"
+                    error_log = error_log +  f" in {self.message.thread.name}"
                 elif self.message.channel:
-                    error_log = error_log +  f"in {self.message.channel.name}"
+                    error_log = error_log +  f" in {self.message.channel.name}"
                 print(error_log)
 
         self.stop()
