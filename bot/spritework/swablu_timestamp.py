f54b6ce2e4006bfde298eaf643cf2d7ccf68c8c9
Doodleboo <174210143+Doodleboo@users.noreply.github.com>
2026-04-25 14:39:05 +0200

    Fix: swablu timestamp message format

--- bot/spritework/swablu_timestamp.py
+++ bot/spritework/swablu_timestamp.py
@@ -10,7 +10,7 @@ MINIMUM_HOURS = 18
 async def send_swablu_timestamp(user: Member, channel: TextChannel | Thread):
     posting_time = datetime.now() + timedelta(hours=MINIMUM_HOURS)
     epoch_time = int(posting_time.timestamp())
-    timestamp_text = f"#- This sprite can be submitted to the gallery <t:{epoch_time}:R> (at <t:{epoch_time}:s>)"
+    timestamp_text = f"-# This sprite can be submitted to the gallery <t:{epoch_time}:R> (at <t:{epoch_time}:s>)"
     prompt_view = HideFeature(user, OptedType.timestamp)
     view_message = await channel.send(content=timestamp_text, view=prompt_view)
     prompt_view.message = view_message
